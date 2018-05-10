378e2298b8f988c708354ed199f7e9c762b80dfc
Parker Fritch <pjfritch1@gmail.com>
2018-05-10 14:41:03 -0700

    move repo out one directory

---
+++ albums/src/components/AlbumDetail.js
@@ -0,0 +1,70 @@
+import React from 'react';
+import { Text, View, Image, Linking } from 'react-native';
+import Card from './Card';
+import CardSection from './CardSection';
+import Button from './Button';
+
+const AlbumDetail = ({ album }) => {
+    const { title, artist, thumbnail_image, image, url } = album;
+    const { 
+        containerStyle, 
+        thumbnailStyle, 
+        thumbnailContainerStyle,
+        headerTextStyle,
+        imageStyle 
+    } = styles;
+    
+    return (
+        <Card>
+            <CardSection>
+                <View style={thumbnailContainerStyle}>
+                    <Image 
+                        style={thumbnailStyle} 
+                        source={{ uri: thumbnail_image }} 
+                    />
+                </View>
+                <View style={containerStyle}>
+                    <Text style={headerTextStyle}>{artist}</Text>
+                    <Text>{title}</Text>
+                </View>
+            </CardSection>
+
+            <CardSection>
+                <Image style={imageStyle} source={{ uri: image }} />
+            </CardSection>
+
+            <CardSection>
+                <Button onPress={() => Linking.openURL(url)} >
+                    Buy Now
+                </Button>
+            </CardSection>
+        </Card>
+    );
+};
+
+const styles = {
+    containerStyle: {
+        flexDirection: 'column',
+        justifyContent: 'space-around'
+    },
+    thumbnailStyle: {
+        height: 50,
+        width: 50
+    },
+    thumbnailContainerStyle: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginLeft: 10,
+        marginRight: 10,
+    },
+    headerTextStyle: {
+        fontSize: 18
+    },
+    imageStyle: {
+        height: 300,
+        flex: 1,
+        width: null
+    }
+};
+
+export default AlbumDetail;
