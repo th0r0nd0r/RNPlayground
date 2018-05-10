eb0f82c9a2c8ed56824d5ffe62e5caaa254c7cb5
Parker Fritch <pjfritch1@gmail.com>
2018-05-10 10:54:49 -0700

    initialize state

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -4,6 +4,8 @@ import axios from 'axios';
 
 
 class AlbumList extends Component {
+    state = { albums: [] };
+
     componentWillMount() {
         axios.get('https://rallycoding.herokuapp.com/api/music_albums')
             .then(response => console.log(response));
