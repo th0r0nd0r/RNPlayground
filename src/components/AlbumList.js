5faca7ae8d0383911049084ab4de745ea6479541
Parker Fritch <pjfritch1@gmail.com>
2018-05-10 11:09:39 -0700

    render album list

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -8,12 +8,21 @@ class AlbumList extends Component {
 
     componentWillMount() {
         axios.get('https://rallycoding.herokuapp.com/api/music_albums')
-            .then(response => console.log(response));
+            .then(response => this.setState({ albums: response.data }));
     }
+
+    renderAlbums() {
+        return this.state.albums.map(album => 
+            <Text key={album.title}>{album.title}</Text>
+        );
+    }
+
     render() {
+        console.log(this.state);
+
         return (
             <View>
-                <Text>Album List</Text>
+                {this.renderAlbums()}
             </View>
         );
     }
