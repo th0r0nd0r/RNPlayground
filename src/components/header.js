2229fbc2138c4fff42ca65497a7802835088915f
Parker Fritch <pjfritch1@gmail.com>
2018-05-09 18:48:33 -0700

    flexbox

--- src/components/header.js
+++ src/components/header.js
@@ -1,13 +1,24 @@
 // Import libraries for making a component
 import React from 'react';
-import { Text } from 'react-native';
+import { Text, View } from 'react-native';
 // Make a component
 const Header = () => {
-    const { textStyle } = styles;
-    return <Text style={textStyle}>Albums</Text>;
+    const { textStyle, viewStyle } = styles;
+    return (
+        <View style={viewStyle}>
+            <Text style={textStyle}>Albums</Text>
+        </View>
+    );
 };
 
 const styles = {
+    viewStyle: {
+        backgroundColor: '#F8F8F8',
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: 60,
+        paddingTop: 15
+    },
     textStyle: {
         fontSize: 20
     }
